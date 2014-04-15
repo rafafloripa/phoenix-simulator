f95605b101f084867b9fcc72461f647bf3edf916
Reza <reza@ooodev.com>
2014-04-15 11:23:51 +0200

    removed the duplicates

--- src/test/java/acceptancetest/util/ATUtillity.java
+++
@@ -1,9 +0,0 @@
-package acceptancetest.util;
-
-import simulator.Simulator;
-import acceptancetest.subscribe.DummyApplication;
-
-public class ATUtillity {
-	public static DummyApplication staticDummyApp;
-	public static Simulator staticSimulator;
-}
