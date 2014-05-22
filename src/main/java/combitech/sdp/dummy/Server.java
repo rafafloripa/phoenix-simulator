9fbbef6c245914b37cb7bc054e2698f215948af0
Christopher Pavlic <christopher.pavlic@combitech.se>
2014-05-22 09:40:18 +0200

    Split up more of the dummy server functionallity so that it becomes easier to use. It now supports startServer help subscribe unsubscribe and exit

--- src/main/java/combitech/sdp/dummy/Server.java
+++ src/main/java/combitech/sdp/dummy/Server.java
@@ -11,7 +11,7 @@ import com.swedspot.vil.policy.AutomotiveCertificate;
 public class Server implements Runnable {
 
 	private AutomotiveManager manager;
-	private Thread serverThread;
+	private static Thread serverThread;
 	private boolean isRunning;
 
 	public Server() {
@@ -45,41 +45,53 @@ public class Server implements Runnable {
 										+ level.getLevel());
 					}
 				});
-		serverThread = new Thread(this);
-		isRunning = true;
-		serverThread.start();
 	}
 
 	public void shutdownServer() {
 		isRunning = false;
 		manager = null;
-		System.exit(1);
 	}
 
 	@Override
 	public void run() {
 		String[] cmd = new String[]{};
 		String cmdLine = "";
+		printWelcomeMessage();
 		while (isRunning) {
-			System.out.println("Enter command");
+			System.out.print("\n\nEnter command > ");
 			cmdLine = System.console().readLine();
 			cmd = cmdLine.split(" ");
 			switch (cmd[0]) {
-			case "network":
+			case "startServer":
+			    startServer();
+			    break;
+			    
+			case "help":
+                printHelp();
+                break;
+			
+			case "network": //AutomotiveManager has no method to tell if a node is connected or not
 				System.out.println("command not supported yet");
 				break;
 
-			case "listen":
-			    for (int i = 1; i < cmd.length; i++) {
-			        try {
-			            manager.register(Integer.parseInt(cmd[i]));
-                    } catch (NumberFormatException e) {
-                        System.out.println("One or more of the parameters are not valid integers: "+cmd[i]);
-                    }
-                }
+			case "subscribe":
+			    if(manager != null){
+			        subscribe(cmd);
+			    } else {
+			        System.out.println("The Server object is null please start the server with startServer");
+			    }
 				break;
+				
+			case "unsubscribe":
+			    if(manager != null){
+			        unsubscribe(cmd);
+                } else {
+                    System.out.println("The Server object is null please start the server with startServer");
+                }
+                break;
 
 			case "exit":
+			    System.out.println("Shutting down server...");
 				shutdownServer();
 				break;
 				
@@ -90,8 +102,68 @@ public class Server implements Runnable {
 		}
 	}
 
-	public static void main(String[] args) {
+	private void printWelcomeMessage() {
+	    System.out.println("\n\n");
+	    System.out.println("Welcome to Combitech's SDP server");
+	    System.out.println("Type help for info on available commands");        
+    }
+
+    private void unsubscribe(String[] cmd) {
+	    for (int i = 1; i < cmd.length; i++) {
+            try {
+                manager.unregister(Integer.parseInt(cmd[i]));
+            } catch (NumberFormatException e) {
+                System.out.println("One or more of the parameters are not valid integers: "+cmd[i]);
+            }
+        }
+    }
+	
+	private void subscribe(String[] cmd) {
+        for (int i = 1; i < cmd.length; i++) {
+            try {
+                manager.register(Integer.parseInt(cmd[i]));
+            } catch (NumberFormatException e) {
+                System.out.println("One or more of the parameters are not valid integers: "+cmd[i]);
+            }
+        }
+    }
+
+    private void printHelp() {
+        String com = "\n\n\t";
+        String des = "\t\t";
+	    System.out.print("\nThe commands are:");
+       
+	    System.out.println(com+"startServer");
+        System.out.println(des+"Starts the server listening on port 9898 and 8251");
+        
+        System.out.println(com+"subscribe <int> <int> <int> ...");
+        System.out.println(des+"The server subscribes for the signals provided as the argument");
+        
+        System.out.println(com+"unsubscribe <int> <int> <int> ...");
+        System.out.println(des+"The server unsubscribes for the signals provided as the argument");
+        
+        System.out.println(com+"exit");
+        System.out.println(des+"The server shuts down and the program exits");
+    }
+
+    
+
+    public static void main(String[] args) {
 		Server dummyServer = new Server();
-		dummyServer.startServer();
+		dummyServer.start();
+		
+		try {
+            serverThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+		System.out.println("Good Bye!");
+        System.exit(1);
 	}
+
+    private void start() {
+        serverThread = new Thread(this);
+        isRunning = true;
+        serverThread.start();        
+    }
 }
