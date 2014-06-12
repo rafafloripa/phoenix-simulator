742151917036748b84ceac1c0f2cd83ad45207c2
Christopher Pavlic <christopher.pavlic@combitech.se>
2014-06-12 20:28:19 +0200

    fixed the removal off gateway nodes. They are now properly closed

--- src/main/java/simulator/Simulator.java
+++ src/main/java/simulator/Simulator.java
@@ -22,30 +22,43 @@ public class Simulator {
 		nodes = new LinkedList<>();
 	}
 
-	public void addAndInitiateNode(String adress, int port) {
+	public boolean addAndInitiateNode(String adress, int port) {
 		SDPNode tmpNode = SDPFactory.createNodeInstance();
 		SDPGatewayNode simulatorGateway = SDPFactory
 				.createGatewayClientInstance();
 		simulatorGateway.init(new SDPNodeEthAddress(adress, port), tmpNode);
 		simulatorGateway.start();
-		simulatorGateways.add(simulatorGateway);
-		nodes.add(SCSFactory.createSCSInstance(tmpNode));
-		simulationState = SimulationState.INITIALIZED;
-	}
-	
-	public void stopAllNodes(){
-		for(SDPGatewayNode node : simulatorGateways){
-			node.stop();
+		try {
+			Thread.sleep(25);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
-		simulatorGateways.removeAll(simulatorGateways);
+		if (!simulatorGateway.connections().isEmpty()) {
+			simulatorGateways.add(simulatorGateway);
+			nodes.add(SCSFactory.createSCSInstance(tmpNode));
+			simulationState = SimulationState.INITIALIZED;
+			return true;
+		}
+		simulatorGateway.stop();
+		return false;
+	}
+
+	public void stopAllNodes() {
+		for (SDPGatewayNode simulatorGateway : simulatorGateways) {
+			for (SDPNode node : simulatorGateway.connections()) {
+				simulatorGateway.disconnect(node);
+			}
+			simulatorGateway.stop();
+		}
+		simulatorGateways.clear();
 	}
 
 	public void provideSignal(int signalID) {
 		for (SCS node : nodes)
 			node.provide(signalID);
 	}
-	
-	public void unprovideSignal(int signalID){
+
+	public void unprovideSignal(int signalID) {
 		for (SCS node : nodes)
 			node.unprovide(signalID);
 	}
@@ -65,10 +78,15 @@ public class Simulator {
 		for (BasicModule module : availableModules)
 			module.stopSimulation();
 	}
-	
-	public void disconnectSimulator(){
-		for (SDPGatewayNode simulatorGateway : simulatorGateways)
+
+	public void disconnectSimulator() {
+		for (SDPGatewayNode simulatorGateway : simulatorGateways) {
+			for (SDPNode node : simulatorGateway.connections()) {
+				simulatorGateway.disconnect(node);
+			}
 			simulatorGateway.stop();
+		}
+		simulatorGateways.clear();
 		simulationState = SimulationState.STOPPED;
 	}
 
@@ -100,8 +118,8 @@ public class Simulator {
 	public void setSimulationState(SimulationState simulationState) {
 		this.simulationState = simulationState;
 	}
-	
-	public void removeModule(BasicModule module){
+
+	public void removeModule(BasicModule module) {
 		availableModules.remove(module);
 	}
 
