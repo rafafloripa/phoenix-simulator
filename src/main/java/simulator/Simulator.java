2a2b29e1acaa28c5a38b9b8ecbc05daee2f01c02
Christopher Pavlic <christopher.pavlic@combitech.se>
2014-06-23 08:56:51 +0200

    added the addAll and removeAll signals methods that retains the ids so that they can be used during disconnect and reconnect

--- src/main/java/simulator/Simulator.java
+++ src/main/java/simulator/Simulator.java
@@ -3,6 +3,7 @@ package simulator;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map.Entry;
 import java.util.concurrent.locks.ReentrantLock;
 
 import android.swedspot.scs.SCS;
@@ -15,6 +16,7 @@ import android.swedspot.sdp.routing.SDPNodeEthAddress;
 
 public class Simulator {
 	private HashMap<Integer, Integer> provideMap;
+	private HashMap<Integer, SCSData> lastValueSent;
 	private LinkedList<SDPGatewayNode> simulatorGateways;
 	private LinkedList<SCS> nodes;
 	private ArrayList<BasicModule> availableModules = new ArrayList<BasicModule>();
@@ -26,6 +28,7 @@ public class Simulator {
 		nodes = new LinkedList<>();
 		provideMap = new HashMap<>();
 		lock = new ReentrantLock();
+		lastValueSent = new HashMap<>();
 	}
 
 	/**
@@ -42,7 +45,7 @@ public class Simulator {
 				.createGatewayClientInstance();
 		simulatorGateway.init(new SDPNodeEthAddress(adress, port), tmpNode);
 		simulatorGateway.start();
-		
+
 		simulatorGateways.add(simulatorGateway);
 		nodes.add(SCSFactory.createSCSInstance(tmpNode));
 		simulationState = SimulationState.INITIALIZED;
@@ -100,6 +103,29 @@ public class Simulator {
 	}
 
 	/**
+	 * Unprovides all signals for all current nodes but retains the IDs
+	 */
+	public void unprovideAll() {
+		for (SCS node : nodes) {
+			for (Entry<Integer, Integer> entry : provideMap.entrySet()) {
+				node.unprovide(entry.getKey());
+			}
+		}
+	}
+
+	/**
+	 * Provides all signals that have not been removed from the simulator for
+	 * all current nodes
+	 */
+	public void provideAll() {
+		for (SCS node : nodes) {
+			for (Entry<Integer, Integer> entry : provideMap.entrySet()) {
+				node.provide(entry.getKey());
+			}
+		}
+	}
+
+	/**
 	 * Sends a single signal to all connected nodes It is important that the
 	 * SCSData match the signal id
 	 * 
@@ -110,6 +136,7 @@ public class Simulator {
 	public void sendValue(int signalID, SCSData data) {
 		for (SCS node : nodes)
 			node.send(signalID, data);
+		lastValueSent.put(signalID, data);
 	}
 
 	/**
@@ -143,6 +170,9 @@ public class Simulator {
 	 * 
 	 */
 	public void disconnectSimulator() {
+		for (Entry<Integer, Integer> entry : provideMap.entrySet()) {
+			unprovideSignal(entry.getKey());
+		}
 		for (SDPGatewayNode simulatorGateway : simulatorGateways) {
 			for (SDPNode node : simulatorGateway.connections()) {
 				simulatorGateway.disconnect(node);
