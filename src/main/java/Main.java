afb1c6363f136ee189673d46860c9398bad6642e
rafafloripa <rafafloripa@gmail.com>
2015-05-08 11:09:31 +0200

    ETS2 - AGA Simulator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,20 @@
 import combitech.sdp.simulator.SimulatorGateway;
-import combitech.sdp.simulator.car.torcs.Torcs;
-import simulator.steeringwheel.gxt27.GXT27Module;
-
-import java.util.Arrays;
+import ets2.EuroTruck;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String ipaddress = "192.168.1.52";
+        String ipaddress = "127.0.0.1";
         SimulatorGateway gateway = new SimulatorGateway();
         gateway.addAndInitiateNode(ipaddress, 8251, null, null);
         gateway.addAndInitiateNode(ipaddress, 9898, null, null);
         gateway.addAndInitiateNode(ipaddress, 9899, null, null);
         Thread.sleep(1000);
-        Torcs torcs = new Torcs(gateway);
-        for (int i : torcs.getProvidingSignals()) {
+        EuroTruck ets2 = new EuroTruck(gateway);
+        for (int i : ets2.getProvidingSignals()) {
             gateway.provideSignal(i);
         }
+        
         Thread.sleep(1000);
-        GXT27Module gtGxt27Module = new GXT27Module(gateway);
-        for (int i : gtGxt27Module.getProvidingSignals()) {
-            gateway.provideSignal(i);
-        }
-        Thread.sleep(1000);
-        gtGxt27Module.startModule();
-        torcs.startModule();
+        ets2.startModule();
     }
 }
---
+++ src/main/java/ets2/EuroTruck.java
@@ -0,0 +1,144 @@
+package ets2;
+
+import android.swedspot.automotiveapi.AutomotiveSignalId;
+import android.swedspot.scs.data.SCSFloat;
+import android.swedspot.scs.data.SCSLong;
+import android.swedspot.scs.data.SCSShort;
+import combitech.sdp.simulator.BasicModule;
+import combitech.sdp.simulator.SimulationModuleState;
+import combitech.sdp.simulator.SimulatorGateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EuroTruck extends BasicModule {
+
+	ServerSocket welcomeSocket;
+	short currentGear;
+	float fuelLevel;
+	float fuelConsumption;
+	float speed;
+	long distance;
+	private float engineSpeed;
+	private float acceleratorPedalPosition;
+	private Socket clientSocket;
+	private final static Logger LOGGER = LoggerFactory
+			.getLogger(EuroTruck.class);
+
+	public EuroTruck(SimulatorGateway gateway) {
+		super(gateway);
+	}
+
+	@Override
+	public int[] getProvidingSignals() {
+		return new int[] { AutomotiveSignalId.FMS_WHEEL_BASED_SPEED,
+				AutomotiveSignalId.FMS_CURRENT_GEAR,
+				AutomotiveSignalId.FMS_FUEL_LEVEL_1,
+				AutomotiveSignalId.FMS_FUEL_RATE,
+				AutomotiveSignalId.FMS_HIGH_RESOLUTION_TOTAL_VEHICLE_DISTANCE,
+				AutomotiveSignalId.FMS_ENGINE_SPEED,
+				AutomotiveSignalId.FMS_ACCELERATOR_PEDAL_POSITION_1 };
+	}
+
+	@Override
+	public void run() {
+		getModuleThread().setName("ETS2");
+		String signalUpdate;
+		while (state == SimulationModuleState.RUNNING) {
+			try {
+				if (welcomeSocket == null) {
+					welcomeSocket = new ServerSocket(6000);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				// System.out.println("Awaiting connection");
+				clientSocket = new Socket("127.0.0.1", 25555);
+				PrintWriter out = new PrintWriter(
+						clientSocket.getOutputStream(), true);
+				out.println("GET /api/ets2/telemetry");
+				out.flush();
+				// System.out.println("Got connection");
+
+				BufferedReader inFromETS2 = new BufferedReader(
+						new InputStreamReader(clientSocket.getInputStream()));
+
+				while (state == SimulationModuleState.RUNNING
+						&& clientSocket.isConnected()) {
+					signalUpdate = inFromETS2.readLine();
+					if (signalUpdate == null) {
+						break;
+					}
+					if (signalUpdate.length() > 30) {
+						extractValues(signalUpdate);
+						gateway.sendValue(
+								AutomotiveSignalId.FMS_WHEEL_BASED_SPEED,
+								new SCSFloat(speed));
+//						System.out.println(speed);
+						 gateway.sendValue(AutomotiveSignalId.FMS_CURRENT_GEAR,
+						 new SCSShort(currentGear));
+//						 System.out.println(currentGear);
+						 gateway.sendValue(AutomotiveSignalId.FMS_FUEL_LEVEL_1,
+						 new SCSFloat(fuelLevel));
+						 gateway.sendValue(AutomotiveSignalId.FMS_FUEL_RATE,
+						 new SCSFloat(fuelConsumption));
+						// gateway.sendValue(AutomotiveSignalId.FMS_HIGH_RESOLUTION_TOTAL_VEHICLE_DISTANCE,
+						// new SCSLong(distance));
+						// gateway.sendValue(AutomotiveSignalId.FMS_ENGINE_SPEED,
+						// new SCSFloat(engineSpeed));
+						// gateway.sendValue(AutomotiveSignalId.FMS_ACCELERATOR_PEDAL_POSITION_1,
+						// new SCSFloat(acceleratorPedalPosition));
+					}
+					Thread.sleep(300);
+				}
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	private void extractValues(String signalUpdate) {
+		String[] values = signalUpdate.split(",");
+
+		try {
+			fuelLevel = Float.parseFloat(values[22].substring(7));
+			fuelLevel /= 8;
+			currentGear = Short.parseShort(values[16].substring(7));
+			speed = Float.parseFloat(values[6].substring(13));
+			fuelConsumption = Float.parseFloat(values[24].substring(25));
+			fuelConsumption *= 100;
+			// distance = Long.parseLong(values[4]);
+			// engineSpeed = Float.parseFloat(values[5]);
+			// acceleratorPedalPosition = Float.parseFloat(values[6]);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void stopModule() {
+		state = SimulationModuleState.STOPPED;
+		try {
+			if (clientSocket != null) {
+				clientSocket.close();
+			}
+			if (welcomeSocket != null) {
+				welcomeSocket.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		super.stopModule();
+	}
+}
